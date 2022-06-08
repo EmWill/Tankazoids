0d61e2b185c1e1aca571703a176fd5b9e0a59eb8
grant <mullinaxgr@gmail.com>
2022-06-08 18:12:55 -0400

    added miles changes

--- Assets/Tankazoids/Parts/HeavenTreads/HeavenTreads.cs
+++ Assets/Tankazoids/Parts/HeavenTreads/HeavenTreads.cs
@@ -7,17 +7,16 @@ using UnityEngine;
 
 public class HeavenTreads : AbstractTread
 {
-    private Tank _myTank;
     private bool _reverse = false;
     private bool _moving = false;
-
-    public float _currBoost = 0f;
-
-    private float _maxBoost = 1.5f;
-    private float _accelTime = 1.5f;
-    private float _decelTime = 0.5f;
+    private float _currBoost = 0f;
+    private float _maxBoost = 2f;
+    private float _accelTime = 2f;
+    private float _decelTime = 2f;
     private float _turnRate = 175f;
-    private float _moveRate = 10;
+    private float _minTurnRate = 75f;
+    private float _moveRate = 5;
+    public List<Sprite> forwardSprites;
 
     private Rigidbody2D _tankRigidbody;
 
@@ -28,11 +27,9 @@ public class HeavenTreads : AbstractTread
     public override void OnEquip(Tank tank)
     {
         base.OnEquip(tank);
-        _myTank = _tank;
 
         // _tankRigidbody = tank.gameObject.GetComponent<Rigidbody2D>();
     }
-
     private void accelerate()
     {
         _currBoost = Mathf.Min(_maxBoost, _currBoost + (_maxBoost / _accelTime) * (float)base.TimeManager.TickDelta);
@@ -42,18 +39,11 @@ public class HeavenTreads : AbstractTread
         _currBoost = Mathf.Max(0f, _currBoost - (_maxBoost / _decelTime) * (float)base.TimeManager.TickDelta);
     }
 
-    public override void GetReconcileData(Writer writer)
-    {
-        writer.WriteSingle(_currBoost);
-    }
-
-    public override void HandleReconcileData(Reader reader)
-    {
-        _currBoost = reader.ReadSingle();
-    }
 
     public override void HandleMovement(Tank.InputData inputData)
     {
+        Debug.Log((float)base.TimeManager.TickDelta);
+
         float direction = 0;
         if (inputData.directionalInput.x > 0.3f)
             direction = -1;
@@ -65,11 +55,11 @@ public class HeavenTreads : AbstractTread
         if (motion > 0.3f)
         {
             motion = 1f;
-            if (direction == 0)
-            {
-                accelerate();
-                accelerating = true;
-            }
+            // if (direction == 0)
+            //{
+            accelerate();
+            accelerating = true;
+            //}
         }
         else if (motion < -0.3f)
         {
@@ -85,20 +75,35 @@ public class HeavenTreads : AbstractTread
         }
         if (direction != 0)
         {
-            float rotationDistance = _turnRate * (float)base.TimeManager.TickDelta;
+            float currentTurnRate = _turnRate;
+            // NEED TO DECELERATE MORE SLOWLY WHEN TURNING FOR THIS TO BE GOOD (or maybe not decelerate AT ALL?)
+            currentTurnRate += (_minTurnRate - _turnRate) * (_currBoost / _maxBoost);
+            float rotationDistance = currentTurnRate * (float)base.TimeManager.TickDelta;
             _tank.transform.Rotate(0, 0, direction * rotationDistance);
         }
-        Vector2 retVal;
+        Vector3 retVal;
         if (!_reverse)
         {
-            retVal = _myTank.transform.position + _myTank.transform.up * motion * (_moveRate + _currBoost * _moveRate) * (float)base.TimeManager.TickDelta;
+            //retVal = _myTank.transform.position + _myTank.transform.up * motion * (_moveRate + _currBoost * _moveRate) * (float)base.TimeManager.TickDelta;
+            retVal = _tank.transform.position + _tank.transform.up * ((_moveRate * motion) + _currBoost * _moveRate) * (float)base.TimeManager.TickDelta;
         }
         else
         {
-            retVal = _myTank.transform.position - _myTank.transform.up * motion * (_moveRate + _currBoost * _moveRate) * (float)base.TimeManager.TickDelta;
+            retVal = _tank.transform.position - _tank.transform.up * motion * (_moveRate + _currBoost * _moveRate) * (float)base.TimeManager.TickDelta;
+            retVal -= _tank.transform.up * _currBoost * _moveRate;
         }
         _moving = motion > 0;
         _tank.transform.position = retVal;
+    }
+
+    public override void GetReconcileData(Writer writer)
+    {
+        writer.WriteSingle(_currBoost);
+    }
+
+    public override void HandleReconcileData(Reader reader)
+    {
+        _currBoost = reader.ReadSingle();
     }
 
     public override float GetCooldown()
